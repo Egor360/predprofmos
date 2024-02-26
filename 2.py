024edf7c36ce6f23e8d978de96db0de2caae69c5
Egor360 <91691005+Egor360@users.noreply.github.com>
2024-02-26 17:44:55 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,30 +1,30 @@
-import csv
-with open('vacancy.csv', encoding='utf-8') as f: #
+import csv # используем встроенную библиотеку csv
+with open('vacancy.csv', encoding='utf-8') as f: # считываем файл
     a = csv.reader(f, delimiter=';')
-    roles = {}
-    company_size = {}
-    salaries = {}
-    companies = []
-    c = 0
+    roles = {} # создаем словарь с профессиями по мин. кол-ву сотрудников, где ключом будут названия компаний
+    company_size = {} # создаем словарь с мин. кол-вом сотрудников, где ключом будут названия компаний
+    salaries = {} # создаем словарь с зарплатами по мин. кол-ву сотрудников в каждой компании, где ключом будут названия компаний
+    companies = [] # создаем список компаний
+    c = 0 # флаг, чтобы пропустить первую строчку
     for line in a:
         if c:
-            try:
+            try: # проверяем если есть в словарях название компании
                 if company_size[line[-1]] < int(line[2]):
                     company_size[line[-1]] = int(line[2])
                     roles[line[-1]] = line[-2]
                     salaries[line[-1]] = line[0]
-            except:
+            except: # иначе добавляем
                 company_size[line[-1]] = int(line[2])
                 roles[line[-1]] = line[-2]
                 salaries[line[-1]] = line[0]
                 if line[-1] not in companies:
                     companies.append(line[-1])
         c = 1
-    min1 = float('inf')
-    com = ''
-    for company in companies:
+    min1 = float('inf') # создадим переменную с макс. значением, в которую позже запишем мин. требуемое значение
+    com = '' # сюда запишем компанию соответствующую значению переменной выше
+    for company in companies: # ищем по компаниям
         if roles[company] == 'классный руководитель':
             if min1 > company_size[company]:
                 min1 = company_size[company]
                 com = company
-    print(f'В компании {com} есть заданная профессия: {roles[com]}, з/п в такой компании составит: {salaries[com]}')
+    print(f'В компании {com} есть заданная профессия: {roles[com]}, з/п в такой компании составит: {salaries[com]}') # выводим
